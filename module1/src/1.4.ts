0115a71b5b570cca4dfc8940f249ede8bc911314
unknown <ajoy9532@gmail.com>
2023-11-02 10:01:34 +0600

    first commit

---
+++ module1/src/1.4.ts
@@ -0,0 +1,22 @@
+// basic data type
+
+// string
+let firstName: string = "Joy";
+// number
+let roll: number = 233;
+// boolean
+let isAdmin: boolean = true;
+// undefined
+let x: undefined = undefined;
+// null
+let y: null = null;
+
+// Array
+let friends: string[] = ["Saif", "Radil"];
+friends.push("Joy");
+
+let eligibleRollList: number[] = [22, 332];
+
+// tuple
+let ageName: [number, string] = [22, "maksudur"];
+let ageNameBoolean: [number, string, boolean] = [22, "maksudur", true];
---
+++ module1/src/1.5.ts
@@ -0,0 +1,26 @@
+// reference type ---> object
+
+const user: {
+  firstName: string;
+  middleName: string;
+  lastName: string;
+} = {
+  firstName: "Maksudur",
+  middleName: "rahman",
+  lastName: "joy",
+};
+
+const userId: {
+  readonly company: "Redon"; // type --> literal type
+  firstName: string;
+  middleName?: string; // optional type
+  lastName: string;
+  isMarried: false;
+} = {
+  company: "Redon",
+  firstName: "Maksudur",
+  lastName: "joy",
+  isMarried: false,
+};
+
+// userId.company = "Phero"; // not work bcz we used type readonly
