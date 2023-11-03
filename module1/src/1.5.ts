c3379606c4740c1b11ef830d9084ea674321b127
unknown <ajoy9532@gmail.com>
2023-11-03 22:31:32 +0600

    Destructuring

--- module1/src/1.5.ts
+++ module1/src/1.5.ts
@@ -1,26 +1,28 @@
-// reference type ---> object
+{
+  // reference type ---> object
 
-const user: {
-  firstName: string;
-  middleName: string;
-  lastName: string;
-} = {
-  firstName: "Maksudur",
-  middleName: "rahman",
-  lastName: "joy",
-};
+  const user: {
+    firstName: string;
+    middleName: string;
+    lastName: string;
+  } = {
+    firstName: "Maksudur",
+    middleName: "rahman",
+    lastName: "joy",
+  };
 
-const userId: {
-  readonly company: "Redon"; // type --> literal type
-  firstName: string;
-  middleName?: string; // optional type
-  lastName: string;
-  isMarried: false;
-} = {
-  company: "Redon",
-  firstName: "Maksudur",
-  lastName: "joy",
-  isMarried: false,
-};
+  const userId: {
+    readonly company: "Redon"; // type --> literal type
+    firstName: string;
+    middleName?: string; // optional type
+    lastName: string;
+    isMarried: false;
+  } = {
+    company: "Redon",
+    firstName: "Maksudur",
+    lastName: "joy",
+    isMarried: false,
+  };
 
-// userId.company = "Phero"; // not work bcz we used type readonly
+  // userId.company = "Phero"; // not work bcz we used type readonly
+}
