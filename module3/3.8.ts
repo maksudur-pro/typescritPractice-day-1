81cbaf229ee935d91294c67c6db7982fc616c766
unknown <ajoy9532@gmail.com>
2023-11-15 21:01:46 +0600

    polymorphism

---
+++ module3/3.8.ts
@@ -0,0 +1,82 @@
+{
+  //
+
+  // polymorphism ==> different different output dei
+
+  class Person {
+    getSleep() {
+      console.log(`i am sleeping for 8 hours per day`);
+    }
+  }
+
+  class Student extends Person {
+    getSleep() {
+      console.log(`i am sleeping for 7 hours per day`);
+    }
+  }
+
+  class Developer extends Person {
+    getSleep() {
+      console.log(`i am sleeping for 6 hours per day`);
+    }
+  }
+
+  const getSleepingHour = (param: Person) => {
+    param.getSleep();
+  };
+
+  const person1 = new Person();
+  const person2 = new Student();
+  const person3 = new Developer();
+  getSleepingHour(person1);
+  getSleepingHour(person2);
+  getSleepingHour(person3);
+
+  class Shape {
+    getArea(): number {
+      return 0;
+    }
+  }
+
+  class Circle extends Shape {
+    radius: number;
+
+    constructor(radius: number) {
+      super();
+      this.radius = radius;
+    }
+
+    getArea(): number {
+      return Math.PI * this.radius * this.radius;
+    }
+  }
+
+  class ReactAngel extends Shape {
+    height: number;
+    width: number;
+
+    constructor(height: number, width: number) {
+      super();
+      this.height = height;
+      this.width = width;
+    }
+
+    getArea(): number {
+      return this.width * this.height;
+    }
+  }
+
+  const getShapeArea = (param: Shape) => {
+    console.log(param.getArea());
+  };
+
+  const shape1 = new Shape();
+  const shape2 = new Circle(10);
+  const shape3 = new ReactAngel(10, 20);
+
+  getShapeArea(shape1);
+  getShapeArea(shape2);
+  getShapeArea(shape3);
+
+  //
+}
