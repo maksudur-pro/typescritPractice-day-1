78f654d13532debc3105c1eb23137061fe5d544f
DESKTOP-4E9T2MH\Admin <ajoy9532@gmail.com>
2024-12-25 23:12:59 +0600

    Object , Optional and Literal Types

---
+++ module1/dist/1.5.js
@@ -0,0 +1,30 @@
+"use strict";
+// Reference Type ---> Object
+// const user: {
+//   firstName: string;
+//   middleName: string;
+//   lastName: string;
+// } = {
+//   firstName: "maksudur",
+//   middleName: "Rahman",
+//   lastName: "Joy",
+// };
+// const user: {
+//   company: "Programming Hero"; // type ---> literal type
+//   firstName: string;
+//   middleName?: string; // optional type --> ?
+//   lastName: string;
+//   isMarried?: boolean;
+// } = {
+//   company: "Programming Hero",
+//   firstName: "maksudur",
+//   lastName: "Joy",
+//   isMarried: false,
+// };
+const user = {
+    company: "Programming Hero",
+    firstName: "maksudur",
+    lastName: "Joy",
+    isMarried: false,
+};
+// user.company = "new company"; Cannot assign to 'company' because it is a read-only property.
