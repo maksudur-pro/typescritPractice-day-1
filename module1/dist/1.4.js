9ea37ea22240b3287555d649d85ffb805b0167a9
DESKTOP-4E9T2MH\Admin <ajoy9532@gmail.com>
2024-12-25 15:45:07 +0600

    Basic data types

---
+++ module1/dist/1.4.js
@@ -0,0 +1,28 @@
+"use strict";
+// basic data type
+// primitive
+// string
+let firstName = "Joy"; // Explicit Type
+// let firstName = "Joy"; // Implicit Type
+// number
+const phoneNumber = 1234;
+// boolean
+const isAdmin = true;
+// undefined
+let x = undefined;
+// null
+let y = null;
+// any type data add kora jai eivabe kora thik na
+let d;
+d = 1234;
+d = "joy";
+d = true;
+d = undefined;
+d = null;
+// non primitive
+// Array
+let friends = ["saif", "radil"];
+let eligibleRollList = [11, 25];
+// tuple --> array --> order --> type of value
+let coordinate = [14, 14];
+let ageName = ["joy", 22, true];
---
+++ module1/src/1.4.ts
@@ -0,0 +1,39 @@
+// basic data type
+
+// primitive
+
+// string
+let firstName: string = "Joy"; // Explicit Type
+// let firstName = "Joy"; // Implicit Type
+
+// number
+const phoneNumber: number = 1234;
+
+// boolean
+const isAdmin: boolean = true;
+
+// undefined
+let x: undefined = undefined;
+
+// null
+let y: null = null;
+
+// any type data add kora jai eivabe kora thik na
+let d;
+d = 1234;
+d = "joy";
+d = true;
+d = undefined;
+d = null;
+
+// non primitive
+
+// Array
+
+let friends: string[] = ["saif", "radil"];
+let eligibleRollList: number[] = [11, 25];
+
+// tuple --> array --> order --> type of value
+
+let coordinate: [number, number] = [14, 14];
+let ageName: [string, number, boolean] = ["joy", 22, true];
