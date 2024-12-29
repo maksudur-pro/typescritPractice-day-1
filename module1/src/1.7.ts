06bae9a0b4ec3eefc8728e6f8914b75898728c13
DESKTOP-4E9T2MH\Admin <ajoy9532@gmail.com>
2024-12-29 15:23:12 +0600

    Spread and Rest Operator

---
+++ module1/src/1.7.ts
@@ -0,0 +1,42 @@
+{
+  //Spread operator
+  //rest operator
+  //destructure
+
+  // learn spread operator
+
+  const bros1: string[] = ["saif", "radil", "hamja"];
+  const bros2: string[] = ["trony", "fahim", "raju"];
+
+  bros1.push(...bros2); // spread operator (...)
+
+  const mentors1 = {
+    typescript: "Joy",
+    redux: "Maksudur",
+    dbms: "Rahman",
+  };
+  const mentors2 = {
+    prisma: "Firoz",
+    next: "Tanmoy",
+    cloud: "Nahid",
+  };
+
+  const mentorsList = {
+    ...mentors1,
+    ...mentors2,
+  };
+
+  // learn rest operator
+
+  //   const greetFriends = (friend1: string, friend2: string, friend3: string) => {
+  //     console.log(`Hi ${friend1} ${friend2} ${friend3}`);
+  //   };
+
+  //   greetFriends("abul", "kuddus", "bulbul", "ubul");
+
+  const greetFriends = (...friends: string[]) => {
+    friends.forEach((friend: string) => console.log(`Hi ${friends}`));
+  };
+
+  greetFriends("abul", "kuddus", "bulbul", "ubul");
+}
