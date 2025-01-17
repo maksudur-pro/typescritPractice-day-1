b9d14daf81e7e8e915ff5dc7852ca805c5a93005
DESKTOP-4E9T2MH\Admin <ajoy9532@gmail.com>
2025-01-17 23:34:48 +0600

    Utility types

---
+++ module2/2.11.ts
@@ -0,0 +1,56 @@
+{
+  //  Utility types
+
+  // pick Utility
+
+  type Person = {
+    name: string;
+    age: number;
+    email?: string;
+    contactNumber: number;
+  };
+
+  type Nameage = Pick<Person, "name" | "age">;
+
+  //   Omit ->bad deya
+  type contactInfo = Omit<Person, "age" | "name">;
+
+  // Required
+
+  type PersonRequired = Required<Person>;
+
+  //Partial --> optional make
+
+  type PersonPartial = Partial<Person>;
+
+  // ReadOnly
+
+  type PersonReadonly = Readonly<Person>;
+
+  const person1: PersonReadonly = {
+    name: "joy",
+    age: 55,
+    contactNumber: 47,
+  };
+
+  //   person1.name = "jof";
+
+  // Record
+
+  //   type MyObj = {
+  //     a: string;
+  //     b: string;
+  //   };
+
+  type MyObj = Record<string, string>;
+
+  const EmptyObj: Record<string, unknown> = {};
+
+  const obj1: MyObj = {
+    a: "ff",
+    b: "dd",
+    c: "fd",
+  };
+
+  //
+}
